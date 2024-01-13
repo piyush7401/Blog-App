db1530981e63ac0210b4a3bdeacfc3880365a4c1
Piyush Varshney <piyush07varshney@gmail.com>
2024-01-14 03:01:14 +0530

    Make the user schema

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//env config
+dotenv.config();
+
+// //router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+
+//mongodb connection
+connectDB();
+
+//rest objecct
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
+
+// Port
+const PORT = 8000;
+//listen
+app.listen(PORT, () => {
+  console.log("server is running");
+});
